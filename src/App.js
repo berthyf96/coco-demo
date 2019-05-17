92b7b23cd86e105e1ad12d144c4da911643fae62
Berthy Feng <bfeng@princeton.edu>
2019-05-17 22:05:40 +0100

    Add app bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import './App.css';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Gallery from 'react-photo-gallery';
 import ImageComponent from './ImageComponent';
 
 import photos from './all_images.json';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#2196f3',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#f50057',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // error: will use the default color
+  },
+});
+
 function App() {
   return (
     <div className="App">
-      <Gallery 
-        photos={photos} 
-        renderImage={ImageComponent}
-      />
+      <MuiThemeProvider theme={theme}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h5" color="inherit">
+              Explore COCO Captions
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Gallery 
+          photos={photos} 
+          renderImage={ImageComponent}
+        />
+      </MuiThemeProvider>
     </div>
   );
 }
